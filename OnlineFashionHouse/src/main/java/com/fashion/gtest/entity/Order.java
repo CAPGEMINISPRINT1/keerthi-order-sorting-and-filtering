ec45d554803eaf94321265c2f12a0ca2f7b35fa4
CAPGEMINISPRINT1 <117802515+CAPGEMINISPRINT1@users.noreply.github.com>
2022-11-15 09:54:34 +0530

    Add files via upload

---
+++ OnlineFashionHouse/src/main/java/com/fashion/gtest/entity/Order.java
@@ -0,0 +1,32 @@
+package com.fashion.gtest.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name ="myorder" )
+public class Order {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String userName;
+	private String orderDate;
+	private int totalOrderCost;
+	private String status;
+	//private List<Product> allProducts;
+	
+	
+}
